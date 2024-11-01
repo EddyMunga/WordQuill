354c25dc964850c26fd7f6cd6f5f7b7c99f1d0db
EddyMunga <eddymwas50@gmail.com>
2024-11-01 09:17:43 +0300

    add database connection

--- src/main/java/com/eddy/wordquill/repository/QuillsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eddy.wordquill.repository;
-
-import com.eddy.wordquill.models.Quill;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface QuillsRepository extends MongoRepository<Quill, String> {
-
-}
